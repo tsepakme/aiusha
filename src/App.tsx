6c7fc4d2d80c566c697ff289d1942f5d7a0d368d
Aiusha <ayushamikhaylov@gmail.com>
2024-12-21 16:25:11 +0400

    Fix indentation in subtitle of App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className='greeting'>
         <h1>Hello world</h1>
-	<h2>this web page is a testing ground where everything is just being tested</h2>
+	      <h2>this web page is a testing ground where everything is just being tested</h2>
       </div>
     </>
   )
