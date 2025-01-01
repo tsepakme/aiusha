c624d9219dc71ce6c7305628528927596b8a6835
Aiusha <ayushamikhaylov@gmail.com>
2025-01-01 14:56:03 +0400

    Add counter functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import './App.scss'
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [count, setCount] = useState(0);
+	const increment = 1;
+  const handleClick = () => {
+    setCount(count + increment);
+  };
+
   return (
     <>
       <div className='greeting'>
         <h1>Hello world</h1>
-				<h2>this web page is a testing ground where everything is just being tested</h2>				
-			</div>
+        <h2>this web page is a testing ground where everything is just being tested</h2>
+        <button onClick={handleClick}>+{increment}</button>
+        <p>Button clicked {count} times</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
