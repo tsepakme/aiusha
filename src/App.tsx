4c245f19962dde5be272eee0102c08cd05d66c9f
Aiusha <ayushamikhaylov@gmail.com>
2025-01-03 21:15:11 +0400

    Add dynamic increment functionality to counter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.scss';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
 	
-	const increment = 1;
   const handleIncrease = () => {
     setCount(count + increment);
   };
@@ -12,11 +12,16 @@ function App() {
     setCount(count - increment);
   };
 
+  const handleIncrementChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIncrement(Number(event.target.value));
+  };
+
   return (
     <>
       <div className='greeting'>
         <h1>Hello world</h1>
         <h2>this web page is a testing ground where everything is just being tested</h2>
+        <input type='number' placeholder='increase/decrease by' value={increment}  onChange={handleIncrementChange}/>
         <button onClick={handleIncrease}>+{increment}</button>
         <button onClick={handleDecrease}>-{increment}</button>
         <p>Button clicked {count} times</p>
