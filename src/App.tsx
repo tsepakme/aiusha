7fdfddaade0f08d965b876708c90202b9c551ebc
Aiusha <ayushamikhaylov@gmail.com>
2025-01-06 15:56:28 +0400

    Add reset functionality to counter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import './App.scss';
 function App() {
   const [count, setCount] = useState(0);
   const [increment, setIncrement] = useState(1);
-	
+
   const handleIncrease = () => {
     setCount(count + increment);
   };
+
   const handleDecrease = () => {
     setCount(count - increment);
   };
@@ -16,14 +17,19 @@ function App() {
     setIncrement(Number(event.target.value));
   };
 
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
     <>
       <div className='greeting'>
         <h1>Hello world</h1>
         <h2>this web page is a testing ground where everything is just being tested</h2>
-        <input type='number' placeholder='increase/decrease by' value={increment}  onChange={handleIncrementChange}/>
+        <input type='number' placeholder='increase/decrease by' value={increment} onChange={handleIncrementChange} />
         <button onClick={handleIncrease}>+{increment}</button>
         <button onClick={handleDecrease}>-{increment}</button>
+        <button onClick={handleReset}>Reset</button>
         <p>Current count: {count}</p>
       </div>
     </>
