71477686f36fd11c1b555255c2e9e9120b5e894a
Aiusha <ayushamikhaylov@gmail.com>
2025-01-10 17:53:54 +0400

    Remove socket.io chat functionality and related components from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,11 @@
-import { useState, useEffect } from 'react';
-import io from 'socket.io-client';
 import './App.scss';
 
-const socket = io('https://aiusha.com'); // Update with your server URL
-
 function App() {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState<string[]>([]);
-
-  useEffect(() => {
-    socket.on('chat message', (msg: string) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
-    });
-
-    return () => {
-      socket.off('chat message');
-    };
-  }, []);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (message.trim()) {
-      socket.emit('chat message', message);
-      setMessage('');
-    }
-  };
 
   return (
     <div className='greeting'>
       <h1>Hello world</h1>
       <h2>this web page is a testing ground where everything is just being tested</h2>
-      <h1>Chat</h1>
-      <ul id="messages">
-        {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input
-          id="m"
-          autoComplete="off"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button>Send</button>
-      </form>
     </div>
   );
 }
