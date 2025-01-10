fcdcbc6d798d0626695fc1df646bfb0034ba226f
Aiusha <ayushamikhaylov@gmail.com>
2025-01-10 17:52:48 +0400

    Update App component to change greeting and enhance page description

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,9 @@ function App() {
   };
 
   return (
-    <div className="chat">
+    <div className='greeting'>
+      <h1>Hello world</h1>
+      <h2>this web page is a testing ground where everything is just being tested</h2>
       <h1>Chat</h1>
       <ul id="messages">
         {messages.map((msg, index) => (
