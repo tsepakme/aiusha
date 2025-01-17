71933602219b743b5503287bbe2407c1d0996909
Aiusha <ayushamikhaylov@gmail.com>
2025-01-17 15:21:38 +0400

    Add TodoContextProps type to define context structure for task management

--- types/todo.ts
+++ types/todo.ts
@@ -3,3 +3,15 @@ export type Task = {
   completed: boolean
   isEditing: boolean
 }
+
+export type TodoContextProps = {
+  tasks: Task[];
+  newTask: string;
+  setNewTask: (task: string) => void;
+  handleAddTask: () => void;
+  handleKeyPress: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  handleToggleTask: (index: number) => void;
+  handleRemoveTask: (index: number) => void;
+  handleEditTask: (index: number) => void;
+  handleSaveTask: (index: number, newText: string) => void;
+}
