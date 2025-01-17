75bbec78d99fe318fda1d8fcc03a10834eb8d72f
Aiusha <ayushamikhaylov@gmail.com>
2025-01-17 18:10:48 +0400

    Refactor useTodos hook by removing commented-out code and improving function clarity

--- hooks/useTodos.tsx
+++ hooks/useTodos.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import { Task } from '../types/todo';
 
-// Пользовательский хук для управления состоянием задач
 const useTodos = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTask, setNewTask] = useState('');
 
-  // Функция для добавления новой задачи
   const handleAddTask = () => {
     if (newTask.trim()) {
       setTasks([...tasks, { text: newTask, completed: false, isEditing: false }]);
@@ -14,14 +12,12 @@ const useTodos = () => {
     }
   };
 
-  // Функция для обработки нажатия клавиши Enter
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       handleAddTask();
     }
   };
 
-  // Функция для переключения состояния выполнения задачи
   const handleToggleTask = (index: number) => {
     const updatedTasks = tasks.map((task, i) =>
       i === index ? { ...task, completed: !task.completed } : task
@@ -29,13 +25,11 @@ const useTodos = () => {
     setTasks(updatedTasks);
   };
 
-  // Функция для удаления задачи
   const handleRemoveTask = (index: number) => {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
   };
 
-  // Функция для начала редактирования задачи
   const handleEditTask = (index: number) => {
     const updatedTasks = tasks.map((task, i) =>
       i === index ? { ...task, isEditing: true } : task
@@ -43,7 +37,6 @@ const useTodos = () => {
     setTasks(updatedTasks);
   };
 
-  // Функция для сохранения отредактированной задачи
   const handleSaveTask = (index: number, newText: string) => {
     const updatedTasks = tasks.map((task, i) =>
       i === index ? { ...task, text: newText, isEditing: false } : task
@@ -51,7 +44,6 @@ const useTodos = () => {
     setTasks(updatedTasks);
   };
 
-  // Функция для обработки нажатия клавиши Enter при редактировании задачи
   const handleEditKeyPress = (event: React.KeyboardEvent<HTMLInputElement>, index: number, newText: string) => {
     if (event.key === 'Enter') {
       handleSaveTask(index, newText);
