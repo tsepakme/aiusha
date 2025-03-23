f747231eebc297a82acf2bc8954f182448b7e186
Aiusha <ayushamikhaylov@gmail.com>
2025-03-23 15:37:33 +0400

    Add HomePage component with tournament systems overview

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Tournament system</h1>
+      <ul>
+        <li>Swiss system</li>
+        <li>Bergvall system</li>
+        <li>Olympic system</li>
+        <li>Round-robin tournament</li>
+      </ul>
+    </div>
+  );
+}
